2d6dce3a38428d265470fc2324e0cee605eff06a
HaftDog <79525002+HaftDog@users.noreply.github.com>
2021-02-23 12:10:00 -0300

    App
    ChatBot

---
+++ main.py
@@ -0,0 +1,287 @@
+import os
+import random
+import time
+
+print("Hi There")
+print("Programmed by a Brazilian programmer")
+print(":)")
+
+name = input("Please write your name here: ")
+
+os.system("cls")
+os.system("clear")
+
+print("Hi",name)
+print("I will teach you how to use this Personal Assistant/ChatBot")
+
+input("Press enter to continue: ")
+
+os.system("cls")
+os.system("clear")
+print("Command example:")
+print("Command: help")
+print("-Commands-")
+print("Here a list of commands will appear")
+print("Now do it yourself")
+
+input("Press enter to continue: ")
+
+os.system("cls")
+os.system("clear")
+
+x=1
+while (x==1):
+  code = input("Command: ")
+
+  if code == "help":
+    print("-Commands-")
+    print("Help: This message will appear")
+    print("Hi: I will answer you :3")
+    print("I use Arch btw: Oh no")
+    print("Clear: Clear the Screen")
+
+  if code == "Help":
+    print("-Commands-")
+    print("Help: This message will appear")
+    print("Hi: I will answer you :3")
+    print("I use Arch btw: Oh no")
+    print("Clear: Clear the Screen")
+
+  if code == "I use Arch btw":
+    iusearchbtw = input("Oh no, is this real? y/n: ")
+    if iusearchbtw == "y":
+      print("Ah hand, I'm going to die, I can't stand it [meme]")
+
+    if iusearchbtw == "Y":
+      print("Ah hand, I'm going to die, I can't stand it [meme]")
+
+    if iusearchbtw == "yes":
+      print("Ah hand, I'm going to die, I can't stand it [meme]")
+    
+    if iusearchbtw == "Yes":
+      print("Ah hand, I'm going to die, I can't stand it [meme]")
+
+    if iusearchbtw == "YES":
+      print("Ah hand, I'm going to die, I can't stand it [meme]")
+
+    if iusearchbtw == "ye":
+      print("Ah hand, I'm going to die, I can't stand it [meme]")
+
+    if iusearchbtw == "YE":
+      print("Ah hand, I'm going to die, I can't stand it [meme]")
+
+    if iusearchbtw == "n":
+      print("Ah, good [meme]")
+
+    if iusearchbtw == "no":
+      print("Ah, good [meme]")
+
+    if iusearchbtw == "not":
+      print("Ah, good [meme]")
+
+    if iusearchbtw == "N":
+      print("Ah, good [meme]")
+
+    if iusearchbtw == "No":
+      print("Ah, good [meme]")
+
+    if iusearchbtw == "Not":
+      print("Ah, good [meme]")
+
+    if iusearchbtw == "NO":
+      print("Ah, good [meme]")
+
+    if iusearchbtw == "NOT":
+      print("Ah, good [meme]")
+
+  if code == "i use Arch btw":
+    iusearchbtw = input("Oh no, is this real? y/n: ")
+    if iusearchbtw == "y":
+      print("Ah hand, I'm going to die, I can't stand it [meme]")
+
+    if iusearchbtw == "Y":
+      print("Ah hand, I'm going to die, I can't stand it [meme]")
+
+    if iusearchbtw == "yes":
+      print("Ah hand, I'm going to die, I can't stand it [meme]")
+    
+    if iusearchbtw == "Yes":
+      print("Ah hand, I'm going to die, I can't stand it [meme]")
+
+    if iusearchbtw == "YES":
+      print("Ah hand, I'm going to die, I can't stand it [meme]")
+
+    if iusearchbtw == "ye":
+      print("Ah hand, I'm going to die, I can't stand it [meme]")
+
+    if iusearchbtw == "YE":
+      print("Ah hand, I'm going to die, I can't stand it [meme]")
+
+    if iusearchbtw == "n":
+      print("Ah, good [meme]")
+
+    if iusearchbtw == "no":
+      print("Ah, good [meme]")
+
+    if iusearchbtw == "not":
+      print("Ah, good [meme]")
+
+    if iusearchbtw == "N":
+      print("Ah, good [meme]")
+
+    if iusearchbtw == "No":
+      print("Ah, good [meme]")
+
+    if iusearchbtw == "Not":
+      print("Ah, good [meme]")
+
+    if iusearchbtw == "NO":
+      print("Ah, good [meme]")
+
+    if iusearchbtw == "NOT":
+      print("Ah, good [meme]")
+
+    if iusearchbtw == "NOT":
+      print("Ah, good [meme]")
+
+  if code == "I use arch btw":
+    iusearchbtw = input("Oh no, is this real? y/n: ")
+    if iusearchbtw == "y":
+      print("Ah hand, I'm going to die, I can't stand it [meme]")
+
+    if iusearchbtw == "Y":
+      print("Ah hand, I'm going to die, I can't stand it [meme]")
+
+    if iusearchbtw == "yes":
+      print("Ah hand, I'm going to die, I can't stand it [meme]")
+    
+    if iusearchbtw == "Yes":
+      print("Ah hand, I'm going to die, I can't stand it [meme]")
+
+    if iusearchbtw == "YES":
+      print("Ah hand, I'm going to die, I can't stand it [meme]")
+
+    if iusearchbtw == "ye":
+      print("Ah hand, I'm going to die, I can't stand it [meme]")
+
+    if iusearchbtw == "YE":
+      print("Ah hand, I'm going to die, I can't stand it [meme]")
+
+    if iusearchbtw == "n":
+      print("Ah, good [meme]")
+
+    if iusearchbtw == "no":
+      print("Ah, good [meme]")
+
+    if iusearchbtw == "not":
+      print("Ah, good [meme]")
+
+    if iusearchbtw == "N":
+      print("Ah, good [meme]")
+
+    if iusearchbtw == "No":
+      print("Ah, good [meme]")
+
+    if iusearchbtw == "Not":
+      print("Ah, good [meme]")
+
+    if iusearchbtw == "NO":
+      print("Ah, good [meme]")
+
+    if iusearchbtw == "NOT":
+      print("Ah, good [meme]")
+
+  if code == "i use arch btw":
+    iusearchbtw = input("Oh no, is this real? y/n: ")
+    if iusearchbtw == "y":
+      print("Ah hand, I'm going to die, I can't stand it [meme]")
+
+    if iusearchbtw == "Y":
+      print("Ah hand, I'm going to die, I can't stand it [meme]")
+
+    if iusearchbtw == "yes":
+      print("Ah hand, I'm going to die, I can't stand it [meme]")
+    
+    if iusearchbtw == "Yes":
+      print("Ah hand, I'm going to die, I can't stand it [meme]")
+
+    if iusearchbtw == "YES":
+      print("Ah hand, I'm going to die, I can't stand it [meme]")
+
+    if iusearchbtw == "ye":
+      print("Ah hand, I'm going to die, I can't stand it [meme]")
+
+    if iusearchbtw == "YE":
+      print("Ah hand, I'm going to die, I can't stand it [meme]")
+
+    if iusearchbtw == "n":
+      print("Ah, good [meme]")
+
+    if iusearchbtw == "no":
+      print("Ah, good [meme]")
+
+    if iusearchbtw == "not":
+      print("Ah, good [meme]")
+
+    if iusearchbtw == "N":
+      print("Ah, good [meme]")
+
+    if iusearchbtw == "No":
+      print("Ah, good [meme]")
+
+    if iusearchbtw == "Not":
+      print("Ah, good [meme]")
+
+    if iusearchbtw == "NO":
+      print("Ah, good [meme]")
+
+    if iusearchbtw == "NOT":
+      print("Ah, good [meme]")
+
+  if code == "Hi":
+    hi = random.randint(0, 1 )
+    if (hi == 0):
+      print("Hi",name,":3, I hope you're having a good day!")
+
+    else :
+      print("Hello friend, hehe")
+
+  if code == "hi":
+    hi = random.randint(0, 1 )
+    if (hi == 0):
+      print("Hi",name,":3, I hope you're having a good day!")
+
+    else :
+      print("Hello friend, hehe")
+
+  if code == "hI":
+    hi = random.randint(0, 1 )
+    if (hi == 0):
+      print("Hi",name,":3, I hope you're having a good day!")
+
+    else :
+      print("Hello friend, hehe")
+
+  if code == "Hello":
+    hi = random.randint(0, 1 )
+    if (hi == 0):
+      print("Hi",name,":3, I hope you're having a good day!")
+
+    else :
+      print("Hello friend, hehe")
+
+  if code == "hello":
+    hi = random.randint(0, 1 )
+    if (hi == 0):
+      print("Hi",name,":3, I hope you're having a good day!")
+
+    else :
+      print("Hello friend, hehe")
+
+  if code == "clear":
+    os.system("cls")
+    os.system("clear")
+
+  if code == "Clear":
+    os.system("cls")
+    os.system("clear")
